a2a2af297ec1c2337bca5c4c3027f47d126296c5
Kris Winer <kriswiner@users.noreply.github.com>
2017-03-25 21:07:28 -0700

    Update BME280_CCS811_Butterfly.ino

--- BME280_CCS811_Butterfly.ino
+++ BME280_CCS811_Butterfly.ino
@@ -16,6 +16,10 @@
  equivalent CO2 concentration in parts per million (400 - 8192 range) and volatile organic componds in parts per billion (0 - 1187).
  The sensor is quite sensitive to breath and other human emissions.
  
+ This sketch was written to operate the air quality sensor using the Butterfly STM32L433 Development Board using Thomas Roell's 
+ STM32L4 Arduino Core. If you are using a different MCU you will have to modify the sketch since the Wire.transfer functions
+ and RTC.h library are specific to the STM32L4.
+ 
  SDA and SCL have 4K7 pull-up resistors (to 3.3V).
  
  Butterfly hardware setup:
