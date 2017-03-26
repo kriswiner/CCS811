2a2133a8a8439e7cd4230c4adf6faf26ee28b32c
Kris Winer <kriswiner@users.noreply.github.com>
2017-03-25 21:05:34 -0700

    Update BME280_CCS811_Butterfly.ino

--- BME280_CCS811_Butterfly.ino
+++ BME280_CCS811_Butterfly.ino
@@ -5,10 +5,10 @@
  find it useful you can buy me a beer some time.
  
  The Bosch BME280 is a simple but high resolution pressure/humidity/temperature sensor, which can be used in its high resolution
- mode but with power consumption of 20 microAmp, or in a lower resolution mode with power consumption of
+ mode with power consumption of 20 microAmp, or in a lower-resolution mode with power consumption of
  only 1 microAmp. The choice will depend on the application.
 
- The AMS CCS811 is an air quality sensor that provides equivalent Co2 and volatile organic measurements from direct
+ The AMS CCS811 is an air quality sensor that provides equivalent CO2 and volatile organic measurements from direct
  I2C register reads as well as current and voltage (effective resistance of the sensing element). Gas sensors, including 
  this MEMs gas sensor in the CCS811 measure resistance of a substrate that changes when exposed to intert gasses and 
  volatile organic compounds. Changed in concentration vary exponentially with the changes in resistance. The CCS811
